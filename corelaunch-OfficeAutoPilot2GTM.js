9dd7bfb0762890ae29bc9760742ba83e984b6575
Austin Pray <austin@austinpray.com>
2014-04-25 22:49:04 -0500

    Fixes jQuery no-conflict
    creates debug methods
    
    fixes jquery.cookie in bower

--- corelaunch-OfficeAutoPilot2GTM.js
+++ corelaunch-OfficeAutoPilot2GTM.js
@@ -4,13 +4,15 @@
  * @api public
  */
 (function(exports){
-  jQuery.cookie.json = true;
+  var $ = jQuery || $;
+  $.cookie.json = true;
   var errorPrefix = "officeAutoPilot2GTM: uhoh... ";
   /** default configuration values */
   var config = {
     'formSelector': 'moonray_order_form form',
     'cookieKey': 'oap2gtmTrans'
   };
+  var productsStore;
 
   /** http://stackoverflow.com/a/2880929/1585343 */
   var urlParams;
@@ -29,14 +31,14 @@
 
   /**
    * Iterate through an array of products and remove dollar signs and format strings as numbers if necessary.
-   * @example 
+   * @example
    * array = hollaHollaGetDollar(array);
    * @param { Array } products - Array of products (probably what you passed into `orderForm()`)
    * @returns { Array } parsed array
    * @api private
    */
   var hollaHollaGetDollar = function (products) {
-    jQuery.each(products, function (i, v) {
+    $.each(products, function (i, v) {
       /* http://stackoverflow.com/a/559178/1585343 */
       v.price = Number(v.price.replace(/[^0-9\.]+/g,""));
     });
@@ -44,8 +46,8 @@
   };
 
   /**
-   * Iterate through an array of products and get the total 
-   * @example 
+   * Iterate through an array of products and get the total
+   * @example
    * array = getTotal(array);
    * @param { Array } products - Array of products (probably what you passed into `orderForm()`)
    * @returns { Number } transaction total
@@ -53,17 +55,17 @@
    */
   var getTotal = function (products) {
     var total = 0;
-    jQuery.each(products, function (i, v) {
+    $.each(products, function (i, v) {
       total = total + (v.price * v.quantity);
     });
     return total;
   };
 
   /**
-   * Call this on the "order form" page where the user inputs their credit card information. 
-   * Pass in an array of "product" objects. When the form with the class `config.formSelector` is submitted, 
+   * Call this on the "order form" page where the user inputs their credit card information.
+   * Pass in an array of "product" objects. When the form with the class `config.formSelector` is submitted,
    * a cookie is saved to the user's browser with the key as `config.cookieKey` and the value as `products`
-   * 
+   *
    * @example
    * officeAutoPilot2GTM.orderForm([
    *    {
@@ -86,28 +88,36 @@
    */
   exports.orderForm = function (products) {
     if(!$.isArray(products)) {
-      console.error(errorPrefix, "products parameter must be an array"); 
+      console.error(errorPrefix, "products parameter must be an array");
     }
-    jQuery('div.' + config.formSelector).submit(function (e) {
-      jQuery.cookie(config.cookieKey, products, { expires: 7 });
+    productsStore = products;
+    $('div.' + config.formSelector).submit(function (e) {
+      exports.setCookie();
     });
   };
 
+  exports.getStore = function () {
+    return productsStore;
+  };
+
+  exports.setCookie = function () {
+    $.cookie(config.cookieKey, productsStore, { expires: 7, path: '/' });
+  };
   /**
    * Call this on the "thank you" page after a successful transaction. It will grab the value stored in the cookie
    * with the key `config.cookieKey`, if it exists, and send those to the GTM datalayer. It will also try and grab any
    * URL parameters sent over from the previous page and inject those into the datalayer call
-   * @example 
+   * @example
    * officeAutoPilot2GTM.send();
    * @api public
    */
   exports.send = function () {
-    var products = jQuery.cookie(config.cookieKey);
+    var products = $.cookie(config.cookieKey);
     if(products !== undefined) {
       products = hollaHollaGetDollar(products);
       dataLayer.push({
         'event': 'trackTrans',
-        'transactionId': urlParams.transaction_id,
+        'transactionId': urlParams.transaction_id || "r"+ Date.now() + "-" + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1),
         'transactionCurrency': 'USD',
         'transactionProducts': products,
         'transactionAffiliation': '',
@@ -115,7 +125,7 @@
         'transactionTax': '',
         'transactionShipping': ''
       });
-      jQuery.removeCookie(config.cookieKey);
+      $.removeCookie(config.cookieKey, { path: '/' });
     }
   };
 
